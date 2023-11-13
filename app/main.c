d09c5c69db9dd67a369680022f7a54697c42af29
Savelii Nikonov <nikonov@cs.petrsu.ru>
2023-11-13 14:26:42 +0300

    coverall

--- app/main.c
+++ app/main.c
@@ -4,6 +4,23 @@
 
 int main()
 {
-    printf("Hello World!\n");
-    return myfunc(2);
+    double *roots;
+
+    printf("Enter number\n");
+    double n = 0;
+    scanf("%lf", &n);
+    printf("SQRT %f\n", my_sqrt(n));
+    printf("Fibonachi %d\n", fibonachi(n));
+    printf("SQRT Fibonachi %f\n", my_sqrt(fibonachi(n)));
+    printf("Enter a, b, c\n");
+    double a = 0;
+    scanf("%lf", &a);
+    double b = 0;
+    scanf("%lf", &b);
+    double c = 0;
+    scanf("%lf", &c);
+    roots = realQuadraticRoots(a, b, c);
+    printf("ROOT1 %f\n", roots[0]);
+    printf("ROOT2 %f\n", roots[1]);
+    free(roots);
 }
