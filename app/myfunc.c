5667bbc7769255b46aee7c2c6d18ef603ad9d6bd
Savelii Nikonov <nikonov@cs.petrsu.ru>
2023-11-13 14:26:42 +0300

    coverall

--- app/myfunc.c
+++ app/myfunc.c
@@ -2,15 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "myfunc.h"
-
-int myfunc(int b) {
-    char *buffer = malloc(sizeof(char) * 1000);
-    buffer [0] = b + 4;
-    // здесь должен ругаться sonarcloud, т.к. утечка памяти
-    return buffer[0];
-}
-
-int val;
+#include <math.h>
 
 int fibonachi(int num) {
     int prev = 1;
@@ -32,7 +24,24 @@ int fibonachi(int num) {
     return next;
 }
 
-void printStdoutMessages() {
-    printf("This is a test message from myfunc.c\n");
-    printf("Do not disturb\n");
+double my_sqrt(double num) {
+    if(num<0){
+       return 0;
+    }
+    return sqrt(num);
+}
+
+double* realQuadraticRoots(double a, double b, double c) {
+    double *roots = malloc(2 * sizeof(double)); 
+    double discriminant = b * b - 4 * a * c;
+    if (discriminant > 0) {
+        roots[0] = (-b + sqrt(discriminant)) / (2 * a);
+        roots[1] = (-b - sqrt(discriminant)) / (2 * a);
+    } else if (discriminant == 0) {
+        roots[0] = -b / (2 * a);
+        roots[1] = roots[0];
+    } else {
+        roots[0] = roots[1] = 0.0;    }
+    
+    return roots;
 }
--- app/myfunc.h
+++ app/myfunc.h
@@ -1,12 +1,8 @@
 #ifndef MYFUNC_H
 #define MYFUNC_H
 
-int myfunc(int b);
-
 int fibonachi(int num);
-
-void printStdoutMessages();
-
-extern int val;
+double my_sqrt(double num);
+double* realQuadraticRoots(double a, double b, double c);
 
 #endif // MYFUNC_H
